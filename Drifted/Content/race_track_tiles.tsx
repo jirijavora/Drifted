9744a770579d9f8154f30f33698729e6adb9f5ac
Jiri Javora <javora.jiri@gmail.com>
2023-10-26 18:25:26 -0500

    Initial track version

---
+++ Drifted/Content/race_track_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="race_track_tiles" tilewidth="128" tileheight="128" spacing="2" tilecount="465" columns="31">
+ <image source="racing_track_tiles.png" width="4096" height="2048"/>
+</tileset>
